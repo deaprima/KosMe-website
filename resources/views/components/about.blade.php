031120bf27a849efb75b5dc2e41764be018e8fc9
deaprima <deaprima209@gmail.com>
2025-05-30 23:39:48 +0700

    add about page

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,5 +1,5 @@
 <!-- About Start -->
-<div id="about" class="py-5 container-xxl">
+<div class="p-0 bg-white container-fluid">
     <div class="container-custom">
         <div class="row g-5 align-items-center">
             <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
@@ -7,9 +7,8 @@
                     <img class="img-fluid w-100" src="{{ asset('assets/img/about.jpg') }}">
                 </div>
             </div>
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+            <div class="p-5 col-lg-6 wow fadeIn" data-wow-delay="0.5s">
                 <h1 class="mb-4">Platform Cari Kos Kekinian, Cepat & Anti Ribet!</h1>
-                </h1>
                 <p class="mb-4">Gak perlu bingung cari tempat tinggal! KosMe hadir buat kamu
                     yang butuh kos nyaman, aman, dan sesuai gaya hidup. Mulai dari kos murah meriah sampai eksklusif,
                     semua bisa kamu temukan dalam satu platform. Cocok untuk mahasiswa, pekerja, atau kamu yang lagi
@@ -18,7 +17,8 @@
                 <p><i class="fa fa-check text-primary me-3"></i>Sistem booking simpel & bisa bayar online</p>
                 <p><i class="fa fa-check text-primary me-3"></i>Ada kos putra, putri, atau campur - pilih sesuai
                     kebutuhan!</p>
-                <a class="px-5 py-3 mt-3 btn btn-primary" href="#listkos">Yuk Jelajahi Kos</a>
+                <a class="px-5 py-3 mt-3 btn btn-primary" href="{{ route('boarding-house.search') }}">Yuk Jelajahi
+                    Kos</a>
             </div>
         </div>
     </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,6 +4,6 @@
     @include('components.jumbotron')
     @include('components.search')
     @include('components.listkos')
-    {{-- @include('components.about') --}}
+    @include('components.about')
     {{-- @include('components.testimonial') --}}
 @endsection
